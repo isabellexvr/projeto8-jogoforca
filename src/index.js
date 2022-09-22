52b2d6946e322396897e04132369e48e07eedaca
isabellexvr <isabellexavier59@gmail.com>
2022-09-22 18:00:14 -0300

    initial

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom";
+import "./styles/reset.css"
+import "./styles/style.css"
+
+function A () {
+    return (
+        <div>olá!</div>
+    )
+}
+
+ReactDOM.render(<A></A>, document.querySelector(".root"))
